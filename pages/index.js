cd260c7cac01de535d158728c4da0fe5fa6c9a12
vivi-shiwei <2546710985@qq.com>
2020-06-02 16:37:53 +0800

    6-2

--- pages/index.js
+++ pages/index.js
@@ -610,7 +610,8 @@ function HomePage() {
                     <ModalHeader>Create your account</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody pb={6}>
-                        <Lorem count={2} />
+                        Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt aute id consequat veniam incididunt duis in sint irure nisi. Mollit officia cillum Lorem ullamco minim nostrud elit officia tempor esse quis.
+                        Sunt ad dolore quis aute consequat. Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
                     </ModalBody>
 
                     <ModalFooter>
